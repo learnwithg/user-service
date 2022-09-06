ff9fe529e7da8459c33d7297bb993d6b228943c5
csulit <chrisgelosulit@gmail.com>
2022-09-06 08:18:19 +0000

    Modified package json version

--- src/shared/config/cors.config.ts
+++ src/shared/config/cors.config.ts
@@ -2,7 +2,5 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 export const CORS_OPTIONS: CorsOptions = {
   origin: ['*'],
-  allowedHeaders:
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
   credentials: true,
 };
